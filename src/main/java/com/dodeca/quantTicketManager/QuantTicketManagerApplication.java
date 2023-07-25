5a34175ad6466dfb1134fea7942227169e80bf37
dodeca05 <kim565898@gmail.com>
2023-07-25 23:26:44 +0900

    init commit

---
+++ src/main/java/com/dodeca/quantTicketManager/QuantTicketManagerApplication.java
@@ -0,0 +1,13 @@
+package com.dodeca.quantTicketManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantTicketManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantTicketManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dodeca/quantTicketManager/QuantTicketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodeca.quantTicketManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantTicketManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
