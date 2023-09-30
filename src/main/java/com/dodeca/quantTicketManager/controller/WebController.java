a39a6089372e8fc313eefcedc93908a9ec0d34df
dodeca05 <kim565898@gmail.com>
2023-09-30 21:51:56 +0900

    도메인 자세히 보기 추가 도메인 생성 추가

--- src/main/java/com/dodeca/quantTicketManager/controller/WebController.java
+++ src/main/java/com/dodeca/quantTicketManager/controller/WebController.java
@@ -2,11 +2,14 @@ package com.dodeca.quantTicketManager.controller;
 
 import com.dodeca.quantTicketManager.database.entity.DomainEntity;
 import com.dodeca.quantTicketManager.service.DomainService;
+import jakarta.websocket.server.PathParam;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -21,6 +24,13 @@ public class WebController {
         List<DomainEntity> domains=domainService.getAllDomain();
         model.addAttribute("domains",domains);
         return "domains";
+    }
+    @GetMapping("/web/domain/{domain}")
+    public String getSpecificDomainWeb(@PathVariable String domain, Model model)
+    {
+        DomainEntity domainEntity=domainService.getDomain(domain);
+        model.addAttribute("domain",domainEntity);
+        return "domain";
     }
     @GetMapping("/web/create")
     public String createDomain() {
